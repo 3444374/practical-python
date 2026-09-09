608f0734348622a050921f91cdab0022e49d3068
Junshun <3444374315@qq.com>
2026-09-09 16:59:16 +0800

    work 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,15 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+i = 0
 
 while principal > 0:
+    if i < 12: 
+        payment = 3684.11
+    else: payment = 2684.11
+
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
+    i = i + 1
 
-print('Total paid', total_paid)
+print('Total paid', total_paid,'Demand months', i + 1)
