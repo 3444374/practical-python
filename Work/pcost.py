77faa45ff9edfacdc75ed1eb4092e289b4cef233
Junshun <3444374315@qq.com>
2026-09-18 16:40:02 +0800

    work 2.4

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,21 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0
 
-with open('./Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        price = float(row[2])
-        shares = int(row[1])
-        total_cost += shares*price
 
-print('Total cost is', total_cost)
+def portfolio_cost(filename):
+    total_cost = 0
+
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        for line in f:
+            row = line.split(',')
+            price = float(row[2])
+            shares = int(row[1])
+            total_cost += shares*price
+
+    return total_cost
+
+if __name__ == "__main__":
+    cost = portfolio_cost('./Data/portfolio.csv')
+    print(cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,18 @@
 # report.py
 #
 # Exercise 2.4
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        header = next(f)
+        for line in f:
+            row = line.split(',')
+            temp = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(temp)
+
+    return portfolio
+
+if __name__ == '__main__':
+    portfolio = read_portfolio('./Data/portfolio.csv')
+    print(portfolio)
